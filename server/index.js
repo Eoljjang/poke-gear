54ec6aebdbb7e737d32978f2f540e640081920f5
Nathan <nathan.wong2002@gmail.com>
2024-12-31 11:50:09 -0700

    mongodb structure

--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@ const express = require('express')
 const cors = require('cors')
 const bodyParser = require('body-parser') // helps with POST-ing.
 const router = require('./routes/router')
+const mongoose = require('mongoose')
+require('dotenv/config')
 
 const app = express()
 
@@ -16,7 +18,12 @@ const corsOptions = {
 app.use(cors(corsOptions))
 app.use('/', router) // router must be at end of file.
 
-const port = 4000 // Backend runs on port 4000. The frontend can run on another port. You query port 4000 to access the server.
+const dbOptions = {useNewUrlParse: true, useUnifiedTopology: true}
+mongoose.connect(process.env.DB_URI, dbOptions)
+.then(() => console.log("DB connected."))
+.catch(err => console.log(err))
+
+const port = process.env.PORT || 4000 // Backend runs on port 4000. The frontend can run on another port. You query port 4000 to access the server.
 const server = app.listen(port, () => {
     console.log(`Server is running on port ${port}`)
 })

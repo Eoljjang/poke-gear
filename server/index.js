1d500c6f7331ab6d0b964da6604d46d03049ac1e
Nathan <nathan.wong2002@gmail.com>
2025-01-03 11:11:22 -0700

    reverted mono repo changes

--- server/index.js
+++ server/index.js
@@ -19,24 +19,6 @@ const corsOptions = {
     allowedHeaders: "Content-Type",
 }
 
-// ------------- Deployment ----------------
-const __dirname1 = path.resolve();
-if(process.env.NODE_ENV === "production"){
-    app.use(express.static(path.join(__dirname1, "../client/dist")))
-    app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname1, "../client/dist", "index.html"))
-    })
-
-}
-else {
-    app.get("/", (req, res) => {
-        res.send("API is running successfully.")
-    })
-}
-
-// ------------- Deployment ----------------
-
-
 app.use(cors(corsOptions))
 app.use('/', router) // router must be at end of file.
 

07b8efef8a508058b8644c1d037556fa9da1d552
Nathan <nathan.wong2002@gmail.com>
2025-01-13 13:00:58 -0700

    removed problematic import

--- server/routes/router.js
+++ server/routes/router.js
@@ -2,7 +2,6 @@
 const express = require('express')
 const router = express.Router()
 const schemas = require('../models/schemas');
-const { useInRouterContext } = require('react-router-dom');
 
 router.get("/", async (req, res) => {
     res.send("Hello world!")

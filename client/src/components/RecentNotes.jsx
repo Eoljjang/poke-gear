52627c0f527c12d5a83ae84247695946c164ebf3
Nathan <nathan.wong2002@gmail.com>
2025-01-15 12:34:17 -0700

    recent note click = navigate

--- client/src/components/RecentNotes.jsx
+++ client/src/components/RecentNotes.jsx
@@ -3,8 +3,23 @@ import { useNavigate, useOutletContext } from 'react-router-dom';
 import { useLocation } from 'react-router-dom';
 import '../styles/components/RecentNotes.css'
 import { formatDate } from '../functions/Helpers';
+
 function RecentNotes(){
     const [userData] = useOutletContext();
+    const navigate = useNavigate();
+    const location = useLocation();
+
+    /*onClick, navigate to: http://localhost:5173/app/notes?userEmail=n&notebook_id=1&note_id=1 */
+    const handleNavigate = (notebook_id, note_id) => {
+        // For URL handling.
+        const queryParams = new URLSearchParams(location.search);
+        const userEmail = queryParams.get("userEmail");
+
+        console.log("Navigate to userEmail:", userEmail, ", notebook_id: ", notebook_id, ", note_id:", note_id);
+        const url = `/app/notes?userEmail=${userEmail}&notebook_id=${notebook_id}&note_id=${note_id}`;
+        navigate(url, {selectedNotebook: notebook_id}); // I need it to also OPEN the note. IE: set the selected states for each notebook :(())
+    }
+
     const output = [] // Array that looks like: [{notebook_id: X, note_id: X, last_edited: X, content: X}, ...]
     userData.forEach((notebook) => {
         notebook.notes.forEach((note) => {
@@ -44,12 +59,12 @@ function RecentNotes(){
                 // 1) notebook_name > note_name
                 // 2) last edited
                 // 3) a short preview of the content
-                return <div key={item.note_id} className='recent-note-item'> {/*onClick, navigate to: http://localhost:5173/app/notes?userEmail=n&notebook_id=1&note_id=1 */}
+                return <div key={item.note_id} className='recent-note-item' onClick={() => handleNavigate(item.notebook_id, item.note_id)}>
                     <div className="note-name">{item.note_name}</div>
 
                     <div className="details-container">
-                        <div className="notebook-name">{item.notebook_name}</div>
-                        <div className="recent-last-edited">{formatDate(item.last_edited)}</div>
+                        <div className="notebook-name"><i>{item.notebook_name}</i></div>
+                        <div className="recent-last-edited"><i>{formatDate(item.last_edited)}</i></div>
                     </div>
                 </div>;
             })}

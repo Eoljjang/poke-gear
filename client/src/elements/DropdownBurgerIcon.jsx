efcd7e85209e7bf13a4f82fe1053445cc9020e56
Nathan <nathan.wong2002@gmail.com>
2025-02-04 13:55:43 -0700

    buger css refactor

--- client/src/elements/DropdownBurgerIcon.jsx
+++ client/src/elements/DropdownBurgerIcon.jsx
@@ -1,17 +1,17 @@
-import '../styles/elements/DropdownBurgerIcon.css'
+import styles from '../styles/elements/DropdownBurgerIcon.module.css'
 import PropTypes from 'prop-types';
 
 const DropdownBurgerIcon = ({onClickOpenSidebar}) => {
     return (
-        <button className='burger-container'
+        <button className={styles['burger-container']}
             onClick={onClickOpenSidebar}
             aria-haspopup="true"
             aria-expanded="false"
             aria-label="Toggle sidebar"
         >
-            <div className="burger-content"/>
-            <div className="burger-content"/>
-            <div className="burger-content"/>
+            <div className={styles["burger-content"]}/>
+            <div className={styles["burger-content"]}/>
+            <div className={styles["burger-content"]}/>
         </button>
     )
 }

aa2bb20617056f0fc520e23999a5d04037b5cb54
Nathan <nathan.wong2002@gmail.com>
2025-02-04 08:22:49 -0700

    bcryptjs instead of bcrypt

--- server/routes/router.js
+++ server/routes/router.js
@@ -2,7 +2,7 @@
 const express = require('express')
 const router = express.Router()
 const schemas = require('../models/schemas');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const jwt = require("jsonwebtoken");
 
 router.get("/", async (req, res) => {

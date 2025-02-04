3e41f41775ad60b5c405df772e20ca27ee2d24d1
Nathan <nathan.wong2002@gmail.com>
2025-02-04 13:45:21 -0700

    quicklinks css refactor

--- client/src/components/QuickLinks.jsx
+++ client/src/components/QuickLinks.jsx
@@ -1,20 +1,20 @@
 import { useState } from "react";
-import "../styles/components/QuickLinks.css"
+import styles from "../styles/components/QuickLinks.module.css"
 function QuickLinks() {
     const [QuickLinks, setQuickLinks] = useState([]); // List contains the current quick links.
 
     // Future feature: Add in quick links here
 
     return(
-        <div className="quickLinks-container">
-            <div className="quickLinks-header">My Dashboard</div>
+        <div className={styles["quickLinks-container"]}>
+            <div className={styles["quickLinks-header"]}>My Dashboard</div>
 
-            <div className="divider"></div>
+            <div className={styles["divider"]}></div>
 
-            <div className="quickLinks-content">
-                <div className="link-item">Link 1 <div className="link-icon">Icon</div></div>
-                <div className="link-item">Link 2 <div className="link-icon">Icon</div></div>
-                <div className="link-item">Link 3 <div className="link-icon">Icon</div></div>
+            <div className={styles["quickLinks-content"]}>
+                <div className={styles["link-item"]}>Link 1 <div className={styles["link-icon"]}>Icon</div></div>
+                <div className={styles["link-item"]}>Link 2 <div className={styles["link-icon"]}>Icon</div></div>
+                <div className={styles["link-item"]}>Link 3 <div className={styles["link-icon"]}>Icon</div></div>
             </div>
         </div>
     )
